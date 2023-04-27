e4ceb3e3b51f880923aa29723b352eab28075e20
Ajdin15 <ajdins@uia.no>
2023-04-27 15:06:30 +0000

    ny versjon

--- tcp_client.go
+++ tcp_client.go
@@ -15,8 +15,9 @@ func main() {
 	}
     
 	log.Println("os.Args[1] = ", os.Args[1])
-
- 	_, err = conn.Write([]byte(os.Args[1]))
+          kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+          log.Println("Kryptert melding: ", string(kryptertMelding))
+ 	_, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,9 +28,9 @@ func main() {
 	} 
 	response := string(buf[:n])
 	log.Printf("reply from proxy: %s", response)
-         
-       kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
-log.Println("Kryptert melding: ", string(kryptertMelding))
-_, err = conn.Write([]byte(string(kryptertMelding)))
+    
+       dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+       log.Println("Dekrypter melding: ", string(dekryptertMelding))
+        _, err = conn.Write([]byte(string(dekryptertMelding)))
 }
 
